0b19d51488ff7a16c61c4ad96a024b34dfb1224d
Noorulain Fahad <Noorulain.Fahad@softwire.com>
2022-08-08 23:05:17 +0100

    Prints the numbers 1 to 100 replacing all multiples of 3 with the word Fizz, multiples of 5 with the word Buzz and the multiples of both 3 and 5 with the word FizzBuzz, Replaces multiples of 7 with Bang, multiples of 7 and 3 with FizzBang, multiples of 7 and 5 with BuzzBang. Replaces multiples of 11 with Bong version 3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,8 @@
 /*function to print the numbers 1 to 100 replacing all multiples of 3 with the word "Fizz", multiples of 5 with the word "Buzz" 
 and the multiples of 3 and 5 with the word "FizzBuzz". Replaces multiples of 7 with "Bang", multiples of 7 and 3 with "FizzBang
-, multples of 7 and 5 with "BuzzBang". Replaces multiples of 11 with Bong*/
+, multples of 7 and 5 with "BuzzBang". Replaces multiples of 11 with Bong.*/
 
-function PrintFizzBuzzBangBong(){
+function PrintFizzBuzzBangBongFezz(){
     for(let i=1; i<=100; i++){
         if(i%3 === 0 && i%5 !==0 && i%7 !==0 && i%11 !==0){ 
             console.log('Fizz \n')
@@ -12,9 +12,9 @@ function PrintFizzBuzzBangBong(){
             console.log('FizzBuzz \n')
         }else if(i%7 === 0 && i%3 !==0 && i%5 !==0 && i%11 !==0){ 
             console.log('Bang \n')
-        }else if(i%3 === 0 && i%7 ===0 && i%5 !==0 && i%11 !==0){ 
+        }else if(i%3 === 0 && i%7 ===0){ 
             console.log('FizzBang \n')
-        }else if(i%3 !== 0 && i%5 ===0 && i%7 ===0 && i%11 !==0){ 
+        }else if(i%5 ===0 && i%7 ===0){ 
             console.log('BuzzBang \n')
         }else if(i%11 === 0){ 
             console.log('Bong \n')
@@ -25,4 +25,4 @@ function PrintFizzBuzzBangBong(){
     }
 }
 
-PrintFizzBuzzBangBong();
+PrintFizzBuzzBangBongFezz();
