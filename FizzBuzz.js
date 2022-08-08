21b2a4e9eabb29e5e542602591974b0519dbe198
Noorulain Fahad <Noorulain.Fahad@softwire.com>
2022-08-08 20:52:03 +0100

    Prints the numbers 1 to 100 replacing all multiples of 3 with the word Fizz, multiples of 5 with the word Buzz and the multiples of both 3 and 5 with the word FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,13 @@
-//function to print the numbers 1 to 100 replacing all multiples of 3 with the word "Fizz"
-function PrintFizz(){
+/*function to print the numbers 1 to 100 replacing all multiples of 3 with the word "Fizz", multiples of 5 with the word "Buzz" 
+and the multiples of 3 and 5 with the word"FizzBuzz" */
+function PrintFizzBuzz(){
     for(let i=1; i<=100; i++){
-        if(i%3 === 0){
+        if(i%3 === 0 && i%5 !==0){
             console.log('Fizz \n')
+        }else if(i%3 !== 0 && i%5 ===0){
+            console.log('Buzz \n')
+        }else if(i%3 === 0 && i%5 === 0){
+            console.log('FizzBuzz \n')
         }else{
             console.log(i + '\n')
         }
@@ -10,4 +15,4 @@ function PrintFizz(){
     }
 }
 
-PrintFizz();
+PrintFizzBuzz();
